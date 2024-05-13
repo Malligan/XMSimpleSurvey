b95073b214efee97a2cba45aeffe558ba66d3e7c
Aliaksandr Haradok <aliaksandr_haradok@epam.com>
2024-05-13 21:24:04 +0200

    Questions feature implementation: navigation, screen composables

--- app/src/main/java/com/hrdk/xmsimplesurvey/MainActivity.kt
+++ app/src/main/java/com/hrdk/xmsimplesurvey/MainActivity.kt
@@ -3,14 +3,27 @@ package com.hrdk.xmsimplesurvey
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.hrdk.xmsimplesurvey.questions.DummyModuleClass
+import com.hrdk.xmsimplesurvey.questions.QuestionsScreen
 import com.hrdk.xmsimplesurvey.ui.theme.XMSimpleSurveyTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,8 +36,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val test = DummyModuleClass()
-                    Greeting("XM Survey ${test.test()}")
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "HomeScreen") {
+                        composable("HomeScreen") { HomeScreen(navController) }
+                        composable("QuestionsScreen") { QuestionsScreen(navController) }
+                    }
                 }
             }
         }
@@ -32,17 +48,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(
+    navController: NavController
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome",
+            modifier = Modifier
+        )
+        Button(
+            onClick = { navController.navigate("QuestionsScreen") },
+        ) {
+            Text(text = "Start survey")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val navController = rememberNavController()
     XMSimpleSurveyTheme {
-        Greeting("Android")
+        HomeScreen(navController)
     }
 }
--- questions/build.gradle.kts
+++ questions/build.gradle.kts
@@ -30,16 +30,29 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
